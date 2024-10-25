434ba9977ef8972bcd4b47ba920d0769d59c5a30
dariusfratila <dariusfratila818@gmail.com>
2024-10-25 16:58:35 +0300

    convert 2d points to 3d & plot

--- extractor.py
+++ extractor.py
@@ -1,7 +1,11 @@
 import cv2
 import numpy as np
+import math
+import matplotlib.pyplot as plt
+import time
+from mpl_toolkits.mplot3d import Axes3D
 from skimage.measure import ransac
-from skimage.transform import FundamentalMatrixTransform, EssentialMatrixTransform
+from skimage.transform import FundamentalMatrixTransform
 
 
 class Extractor:
@@ -9,8 +13,16 @@ class Extractor:
         self.orb = cv2.ORB_create()
         self.bg = cv2.BFMatcher(cv2.NORM_HAMMING, crossCheck=False)
         self.last = None
+        self.camPos = [0, 0, 0]
+        self.cam_xyz = []
+        self.lm_xyz = []
+        self.scale = 2
+        self.all_landmarks = []
+        self.all_camera_positions = []
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111, projection='3d')
+        plt.ion()
 
-    # turn [x, y] -> [x, y, 1]
     def add_ones(self, x):
         return np.concatenate([x, np.ones((x.shape[0], 1))], axis=1)
 
@@ -28,8 +40,6 @@ class Extractor:
             kps.append(feat)
 
         kps, des = self.orb.compute(img, kps)
-
-        # print(des.shape)
         matched_keypoints = []
 
         if self.last is not None:
@@ -43,7 +53,7 @@ class Extractor:
         if len(matched_keypoints) > 0:
             matched_keypoints = np.array(matched_keypoints)
             model, inliers = ransac((matched_keypoints[:, 0], matched_keypoints[:, 1]),
-                                    EssentialMatrixTransform,
+                                    FundamentalMatrixTransform,
                                     min_samples=8,
                                     residual_threshold=1,
                                     max_trials=100)
@@ -51,3 +61,71 @@ class Extractor:
 
         self.last = {'kps': kps, 'des': des}
         return matched_keypoints
+
+    def pointClouds(self, W, H, points1, points2):
+        W, H = W // 2, H // 2
+
+        # focal length
+        fov = 60 * (math.pi / 180)
+        f_w = W / math.tan(fov / 2)
+        f_h = H / math.tan(fov / 2)
+
+        # camera matrix parameters
+        K = np.array([[f_w, 0, W],
+                     [0, f_h, H],
+                     [0, 0, 1]])
+
+        E, mask = cv2.findFundamentalMat(np.float32(points2),
+                                         np.float32(points1),
+                                         cv2.FM_8POINT)
+
+        # rotation and translation
+        points, R, t, mask = cv2.recoverPose(E, np.float32(points2),
+                                             np.float32(points1), K, 500)
+        R = np.asmatrix(R).I
+
+        # update camera position
+        new_cam_pos = [self.camPos[0] + t[0][0],
+                       self.camPos[1] + t[1][0],
+                       self.camPos[2] + t[2][0]]
+        self.all_camera_positions.append(new_cam_pos)
+
+        # camera matrix
+        C = np.hstack((R, t))
+        P = np.asmatrix(K) * np.asmatrix(C)
+
+        frame_landmarks = []
+        for i in range(len(points2)):
+            x_i = np.asmatrix([points2[i][0], points2[i][1], 1]).T
+            X_i = np.asmatrix(P).I * x_i
+            landmark = [X_i[0][0] * self.scale + self.camPos[0],
+                        X_i[1][0] * self.scale + self.camPos[1],
+                        X_i[2][0] * self.scale + self.camPos[2]]
+            frame_landmarks.append(landmark)
+
+        self.all_landmarks.extend(frame_landmarks)
+        self.camPos = new_cam_pos
+
+    def update_plot(self):
+        self.ax.cla()
+
+        landmarks = np.array(self.all_landmarks)
+        camera_positions = np.array(self.all_camera_positions)
+
+        if len(landmarks) > 0:
+            self.ax.scatter(landmarks[:, 0], landmarks[:, 1], landmarks[:, 2],
+                            c='b', s=1, label='Landmarks')
+
+        if len(camera_positions) > 0:
+            self.ax.scatter(camera_positions[:, 0], camera_positions[:, 1],
+                            camera_positions[:, 2], c='r', s=50,
+                            label='Camera Positions')
+            self.ax.plot(camera_positions[:, 0], camera_positions[:, 1],
+                         camera_positions[:, 2], 'r-', linewidth=1)
+
+        self.ax.set_xlabel('X')
+        self.ax.set_ylabel('Y')
+        self.ax.set_zlabel('Z')
+        self.ax.legend()
+        plt.draw()
+        plt.pause(0.001)
--- main.py
+++ main.py
@@ -18,13 +18,25 @@ def process_frame(img):
     # keypoints = fe.extract(img)
     matches = fe.extract(img)
     print(f'matches: {len(matches)}')
+
+    if len(matches):
+        points1 = matches[:, 0] # type: ignore
+        points2 = matches[:, 1] # type: ignore
+        fe.pointClouds(W, H, points1, points2)
+
     for pt1, pt2 in matches:
         u1, v1 = map(lambda x: int(round(x)), pt1)
         u2, v2 = map(lambda x: int(round(x)), pt2)
+        print(f'u1, v1: {u1, v1}')
+        print(f'u2, v2: {u2, v2}')
         cv2.circle(img, (u1, v1), color=(0, 255, 0), radius=3)
+        # cv2.circle(img, (u2, v2), color=(255, 0, 0), radius=1)
+
         cv2.line(img, (u1, v1), (u2, v2), color=(255, 0, 0))
 
     disp.paint(img)
+
+# def buildMap(matches, )
 
 
 if __name__ == "__main__":
@@ -35,9 +47,14 @@ if __name__ == "__main__":
     d = fe.add_ones(x)
     print(d)
 
+    frame_counter = 0
     while cap.isOpened():
         ret, frame = cap.read()
         if ret == True:
             process_frame(frame)
+            frame_counter += 1
+
+            if frame_counter % 50 == 0:
+                fe.update_plot()
         else:
             break
